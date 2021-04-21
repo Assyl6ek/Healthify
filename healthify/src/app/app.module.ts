c1f1913c52636ad44c124e51e34cf64ec5908f40
Asyl6ek <47147092+Asyl6ek@users.noreply.github.com>
2021-04-21 23:52:20 +0600

    First Part

--- healthify/src/app/app.module.ts
+++ healthify/src/app/app.module.ts
@@ -3,16 +3,51 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { SearchComponent } from './search/search.component'
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MainComponent } from './main/main.component';
+import { DocCardComponent } from './doc-card/doc-card.component';
+import { DoctorProperDetailsComponent } from './doctor-proper-details/doctor-proper-details.component';
+import {MatInputModule} from '@angular/material/input';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatNativeDateModule} from '@angular/material/core'
+import {MatFormFieldModule} from '@angular/material/form-field'
+import { FormsModule } from '@angular/forms';
+import { LoginComponent } from './login/login.component';
+import { AdminComponent } from './admin/admin.component';
+import { AssortedDoctorsListComponent } from './assorted-doctors-list/assorted-doctors-list.component'
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    AdminComponent,
+    DocCardComponent,
+    DoctorProperDetailsComponent,
+    LoginComponent,
+    MainComponent,
+    AssortedDoctorsListComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatSelectModule,
+    HttpClientModule,
+    MatButtonModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatNativeDateModule,
+    FormsModule
+
   ],
-  providers: [],
+  providers: [
+    MatDatepickerModule
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ healthify/src/app/assorted-doctors-list/assorted-doctors-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-assorted-doctors-list',
+  templateUrl: './assorted-doctors-list.component.html',
+  styleUrls: ['./assorted-doctors-list.component.css']
+})
+export class AssortedDoctorsListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
